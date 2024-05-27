1cbb72113da6bdc4fb64b0d96055059332ad691a
FB99FIRE <felix99@hotmail.nl>
2024-05-27 14:03:07 +0200

    first-activity-wat-vind-ik-erger

---
+++ app/src/main/java/com/example/codeacadamygroepje/userInterface/WatVindIkErgerActivity.java
@@ -0,0 +1,26 @@
+package com.example.codeacadamygroepje.userInterface;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.codeacadamygroepje.R;
+
+public class WatVindIkErgerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_wat_vind_ik_erger);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}

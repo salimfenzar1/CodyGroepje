c423d20bc7c954f826bf8101bfda3c328f85380a
FB99FIRE <felix99@hotmail.nl>
2024-05-27 14:19:13 +0200

    update

--- app/src/main/java/com/example/codeacadamygroepje/userInterface/WatVindIkErgerActivity.java
+++ app/src/main/java/com/example/codeacadamygroepje/userInterface/WatVindIkErgerActivity.java
@@ -16,6 +16,7 @@ public class WatVindIkErgerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        //
         setContentView(R.layout.activity_wat_vind_ik_erger);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());

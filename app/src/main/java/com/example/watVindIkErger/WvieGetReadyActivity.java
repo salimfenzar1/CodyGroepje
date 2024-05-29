bd53c61fd14507767b634f7a0da41f9bfdef0ac9
Beatricepothof <ba.pothof@student.avans.nl>
2024-05-29 12:12:09 +0200

    fixes + temporary randomized choice from get ready to choice red/yellow

--- app/src/main/java/com/example/watVindIkErger/WvieGetReadyActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieGetReadyActivity.java
@@ -1,6 +1,12 @@
 package com.example.watVindIkErger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.SpeechHelper;
 import com.example.codycactus.R;
 
 public class WvieGetReadyActivity extends AppCompatActivity {
+    private SpeechHelper speechHelper;
+    private ImageButton next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,16 @@ public class WvieGetReadyActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        next = findViewById(R.id.nextButton);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "je hebt op de volgende pagina gedrukt", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), WvieStatementRedActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }

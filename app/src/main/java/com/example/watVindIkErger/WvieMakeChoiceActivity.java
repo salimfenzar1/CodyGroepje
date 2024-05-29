94dba37a1931e06d32071aeb8cb596b9cc07ca50
FB99FIRE <felix99@hotmail.nl>
2024-05-29 13:32:48 +0200

    fix make choice activity

--- app/src/main/java/com/example/watVindIkErger/WvieMakeChoiceActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieMakeChoiceActivity.java
@@ -2,6 +2,7 @@ package com.example.watVindIkErger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
@@ -10,11 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.SpeechHelper;
 import com.example.codycactus.R;
 
 import java.util.Random;
 
 public class WvieMakeChoiceActivity extends AppCompatActivity {
+    private SpeechHelper speechHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +29,33 @@ public class WvieMakeChoiceActivity extends AppCompatActivity {
             return insets;
         });
 
-        // random boolean will be generated
-        Random random = new Random();
-        // starting activity based on what boolean is generated
-        Intent intent;
-        if (random.nextBoolean()) {
-            intent = new Intent(this, WvieChoiceRedActivity.class);
-        } else {
-            intent = new Intent(this, WvieChoiceYellowActivity.class);
-        }
-        startActivity(intent);
+        new Handler().postDelayed(() -> {
+            // random boolean will be generated
+            Random random = new Random();
+            // starting activity based on what boolean is generated
+            Intent intent;
+            if (random.nextBoolean()) {
+                intent = new Intent(this, WvieChoiceRedActivity.class);
+            } else {
+                intent = new Intent(this, WvieChoiceYellowActivity.class);
+            }
+            startActivity(intent);
+            finish();
+        }, 8000);
+        new Handler().postDelayed(this::speakText, 2000);
+    }
+    public void speakText(){
+        speechHelper = new SpeechHelper(this);
+        speechHelper.speak(" Kies nu aan welke kant van mij je gaat staan", new SpeechHelper.SpeechCompleteListener() {
+            @Override
+            public void onSpeechComplete() {
+                Log.d("Speech", "Speech synthesis voltooid");
+            }
+
+            @Override
+            public void onSpeechFailed() {
+                Log.e("Speech", "Speech synthesis mislukt");
+            }
+        });
     }
 }

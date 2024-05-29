5ea7df270fb17bc7be6313cadde419767f0e33db
Beatricepothof <ba.pothof@student.avans.nl>
2024-05-29 14:02:14 +0200

    added intent from end game back to home

--- app/src/main/java/com/example/watVindIkErger/WvieGameEndActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieGameEndActivity.java
@@ -1,6 +1,10 @@
 package com.example.watVindIkErger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.MainActivity;
 import com.example.codycactus.R;
 
 public class WvieGameEndActivity extends AppCompatActivity {
+    private ImageButton home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,16 @@ public class WvieGameEndActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        home = findViewById(R.id.homeButton);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "je hebt op home geklikt", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }

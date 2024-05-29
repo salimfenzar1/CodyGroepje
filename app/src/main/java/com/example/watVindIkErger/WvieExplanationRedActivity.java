4c2888cc46c46a4e2b8303a9702fda91a57f20e1
FB99FIRE <felix99@hotmail.nl>
2024-05-29 12:21:34 +0200

    feature explanation activities

--- app/src/main/java/com/example/watVindIkErger/WvieExplanationRedActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieExplanationRedActivity.java
@@ -1,6 +1,12 @@
 package com.example.watVindIkErger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.SpeechHelper;
 import com.example.codycactus.R;
 
 
 public class WvieExplanationRedActivity extends AppCompatActivity {
+    private SpeechHelper speechHelper;
+    private ImageButton next;
+    private boolean selectedYes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,38 @@ public class WvieExplanationRedActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Intent intent = getIntent();
+        if (intent != null) {
+            // Get the string with the key "key"
+            selectedYes = intent.getBooleanExtra("selectedYes", false);
+
+        }
+        new Handler().postDelayed(this::speakText, 2000);
+
+        next = findViewById(R.id.nextButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "je hebt op de volgende pagina gedrukt", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), WvieOtherOpinionsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void speakText(){
+        speechHelper = new SpeechHelper(this);
+        String textToSpeak = selectedYes ? "Waarom vindt je deze stelling erger?" : "Waarom vindt je de gele stelling erger?";
+        speechHelper.speak(textToSpeak, new SpeechHelper.SpeechCompleteListener() {
+            @Override
+            public void onSpeechComplete() {
+                Log.d("Speech", "Speech synthesis voltooid");
+            }
+
+            @Override
+            public void onSpeechFailed() {
+                Log.e("Speech", "Speech synthesis mislukt");
+            }
         });
     }
 }
--- app/src/main/java/com/example/watVindIkErger/WvieExplanationYellowActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieExplanationYellowActivity.java
@@ -1,6 +1,12 @@
 package com.example.watVindIkErger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.SpeechHelper;
 import com.example.codycactus.R;
 
 public class WvieExplanationYellowActivity extends AppCompatActivity {
+    private SpeechHelper speechHelper;
+    private ImageButton next;
+    private boolean selectedYes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,44 @@ public class WvieExplanationYellowActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Intent intent = getIntent();
+        if (intent != null) {
+            // Get the string with the key "key"
+            selectedYes = intent.getBooleanExtra("selectedYes", false);
+
+//            // Check if the value is "Ja" or "Nee" and do something with it
+//            if (selectedYes) {
+//                Toast.makeText(this, "Received value: Yes", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "Received value: No", Toast.LENGTH_SHORT).show();
+//            }
+        }
+        new Handler().postDelayed(this::speakText, 2000);
+
+        next = findViewById(R.id.nextButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "je hebt op de volgende pagina gedrukt", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), WvieOtherOpinionsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void speakText(){
+        speechHelper = new SpeechHelper(this);
+        String textToSpeak = selectedYes ? "Waarom vindt je deze stelling erger?" : "Waarom vindt je de rode stelling erger?";
+        speechHelper.speak(textToSpeak, new SpeechHelper.SpeechCompleteListener() {
+            @Override
+            public void onSpeechComplete() {
+                Log.d("Speech", "Speech synthesis voltooid");
+            }
+
+            @Override
+            public void onSpeechFailed() {
+                Log.e("Speech", "Speech synthesis mislukt");
+            }
         });
     }
 }

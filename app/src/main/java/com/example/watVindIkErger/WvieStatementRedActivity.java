d6ff0dfb132b5a60229f67f3ed1e730e0e47a939
Beatricepothof <ba.pothof@student.avans.nl>
2024-05-29 11:18:45 +0200

    Added onclicklistener for statement red to statement yellow

--- app/src/main/java/com/example/watVindIkErger/WvieStatementRedActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieStatementRedActivity.java
@@ -1,6 +1,10 @@
 package com.example.watVindIkErger;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,8 @@ import com.example.codycactus.R;
 
 public class WvieStatementRedActivity extends AppCompatActivity {
 
+    private ImageButton next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,16 @@ public class WvieStatementRedActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        next = findViewById(R.id.nextButton);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "je hebt op de volgende pagina gedrukt", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), WvieStatementYellowActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/watVindIkErger/WvieStatementYellowActivity.java
+++ app/src/main/java/com/example/watVindIkErger/WvieStatementYellowActivity.java
@@ -21,5 +21,6 @@ public class WvieStatementYellowActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }

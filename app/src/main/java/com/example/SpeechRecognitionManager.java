3909e3b68ae0e17803caf232e59cc7f8c877daef
salimfenzar <s.fenzar@student.avans.nl>
2024-06-03 13:17:36 +0200

    changeds

--- app/src/main/java/com/example/SpeechRecognitionManager.java
+++ app/src/main/java/com/example/SpeechRecognitionManager.java
@@ -92,15 +92,16 @@ public class SpeechRecognitionManager {
 
     public void startListening() {
         if (speechRecognizer != null && !isListening) {
-        if (SpeechRecognizer.isRecognitionAvailable(context)) {
-            Log.d("StartListening Method", "Listening...");
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.forLanguageTag("nl-NL"));
-            intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Zeg iets...");
-            speechRecognizer.startListening(intent);
-            isListening = true;
+            if (SpeechRecognizer.isRecognitionAvailable(context)) {
+                Log.d("StartListening Method", "Listening...");
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.forLanguageTag("nl-NL"));
+                intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Zeg iets...");
+                speechRecognizer.startListening(intent);
+                isListening = true;
+            }
         }
     }
 

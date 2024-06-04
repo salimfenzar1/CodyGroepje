84cf816d2679c7bcd10d0ccbffb22cd5f4ba58c6
Amin-Ahmidout <AhmidoutA@hotmail.com>
2024-06-04 12:29:26 +0200

    Statements toegevoegd voor overlijden

--- app/src/main/java/com/example/DAO/DatabaseInitializer.java
+++ app/src/main/java/com/example/DAO/DatabaseInitializer.java
@@ -94,7 +94,69 @@ public class DatabaseInitializer {
                 statement10.intensityLevel = 4;
                 statementDAO.insert(statement10);
 
-                // Insert more statements as needed
+                // Overlijden
+                Statement statement11 = new Statement();
+                statement1.description = "Wanneer een cliënt mogelijk bijnnkomst krijnt te overlijden en hier vragen over heeft, vind ik het lastig om hier mee om te gaan.";
+                statement1.category = "Overlijden";
+                statement1.imageUrl = ""; // Geen afbeelding gegeven
+                statement1.intensityLevel = 3;
+                statementDAO.insert(statement1);
+
+                Statement statement12 = new Statement();
+                statement2.description = "Na het overlijden van een cliënt die zelf regelmatig zorg weigerde voel ik me erg schuldig en machteloos.";
+                statement2.category = "Overlijden";
+                statement2.imageUrl = ""; // Geen afbeelding gegeven
+                statement2.intensityLevel = 4;
+                statementDAO.insert(statement2);
+
+                Statement statement13 = new Statement();
+                statement3.description = "Ik vind het lastig wanneer familie van een, reeds overleden, cliënt langs komt. Als ik hun verhaal zie weet ik niet wat ik tegen hen moet zeggen.";
+                statement3.category = "Overlijden";
+                statement3.imageUrl = ""; // Geen afbeelding gegeven
+                statement3.intensityLevel = 3;
+                statementDAO.insert(statement3);
+
+                Statement statement14 = new Statement();
+                statement4.description = "Na het overlijden van een cliënt heb ik wel eens te maken gehad met een stress-reactie, zoals: tinteling, verre/pretlichtjes of vermoeidheid in het lichaam.";
+                statement4.category = "Overlijden";
+                statement4.imageUrl = ""; // Geen afbeelding gegeven
+                statement4.intensityLevel = 2;
+                statementDAO.insert(statement4);
+
+                Statement statement15 = new Statement();
+                statement5.description = "Na het overlijden van een cliënt, waar ik een hechte band mee had voelt ik me wel eens eenzaam.";
+                statement5.category = "Overlijden";
+                statement5.imageUrl = ""; // Geen afbeelding gegeven
+                statement5.intensityLevel = 4;
+                statementDAO.insert(statement5);
+
+                Statement statement16 = new Statement();
+                statement6.description = "Het delen van ervaringen is volgens onderzoek essentieel voor een gezonde verwerking van gebeurtenissen. Ik kan mijn ervaringen goed delen met de mensen om me heen.";
+                statement6.category = "Overlijden";
+                statement6.imageUrl = ""; // Geen afbeelding gegeven
+                statement6.intensityLevel = 2;
+                statementDAO.insert(statement6);
+
+                Statement statement17 = new Statement();
+                statement7.description = "Wanneer een cliënt een dierbare verliest vind ik het lastig om hen hiermee te begroederen.";
+                statement7.category = "Overlijden";
+                statement7.imageUrl = ""; // Geen afbeelding gegeven
+                statement7.intensityLevel = 3;
+                statementDAO.insert(statement7);
+
+                Statement statement18 = new Statement();
+                statement8.description = "Wanneer ik met collega's praat over het overlijden van een cliënt merk ik dat zij de dood als een onvermijdelijk onderdeel van het leven zien en hier minder moeite mee hebben. Ik voel me daardoor soms wat begrensd of beloodt in mijn eigen gevoelens.";
+                statement8.category = "Overlijden";
+                statement8.imageUrl = ""; // Geen afbeelding gegeven
+                statement8.intensityLevel = 3;
+                statementDAO.insert(statement8);
+
+                Statement statement19 = new Statement();
+                statement9.description = "Ik ben wel eens geschrokken omdat er niet met mij was gecommuniceerd over het overlijden van een cliënt.";
+                statement9.category = "Overlijden";
+                statement9.imageUrl = ""; // Geen afbeelding gegeven
+                statement9.intensityLevel = 3;
+                statementDAO.insert(statement9);
 
                 // Mark the database as populated
                 SharedPreferences.Editor editor = preferences.edit();

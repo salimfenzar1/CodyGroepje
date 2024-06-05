890847f5deff4da641b790c1f2d886781362fdd9
Beatricepothof <ba.pothof@student.avans.nl>
2024-06-05 11:59:33 +0200

    small fix

--- app/src/test/java/com/example/codycactus/DatabaseUnitTest/RoomDatabaseConnectionUnitTest.java
+++ app/src/test/java/com/example/codycactus/DatabaseUnitTest/RoomDatabaseConnectionUnitTest.java
@@ -15,7 +15,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
+@Config(manifest=Config.NONE)
 @RunWith(RobolectricTestRunner.class)
 public class RoomDatabaseConnectionUnitTest {
 

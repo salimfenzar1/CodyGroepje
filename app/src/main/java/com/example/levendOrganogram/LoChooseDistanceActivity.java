7915adbfc5c2851f4ac21f1d110b371dc8809d0d
FB99FIRE <felix99@hotmail.nl>
2024-06-12 11:43:56 +0200

    fix code

--- app/src/main/java/com/example/levendOrganogram/LoChooseDistanceActivity.java
+++ app/src/main/java/com/example/levendOrganogram/LoChooseDistanceActivity.java
@@ -17,7 +17,7 @@ public class LoChooseDistanceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lo_choose_distance);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lo_choose_distance), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;

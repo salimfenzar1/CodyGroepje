069e7ff2de4730a712966a318068244d26ec1d8a
Beatricepothof <ba.pothof@student.avans.nl>
2024-06-12 13:20:29 +0200

    Timer added for LoChooseDistanceActivity

--- app/src/main/java/com/example/levendOrganogram/LoChoicesActivity.java
+++ app/src/main/java/com/example/levendOrganogram/LoChoicesActivity.java
@@ -17,7 +17,7 @@ public class LoChoicesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lo_choices);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.lo_choices), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
